c0dbe542ab97da57d57288ad140532161ae2e9b7
Ryan The <ryan.the.2006@gmail.com>
2020-03-10 06:53:55 +0800

    Convert to typescript

---
+++ react-native.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}
+  },
+  dependencies: {
+    "react-native-code-push": {
+      platforms: {
+        android: null
+      }
+    },
+    'react-native-notifications': {
+      platforms: {
+        android: null,
+      },
+    },
+  },
+  assets: ["./assets/fonts"]
+};
