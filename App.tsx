37b240d7c9f4565509be17f79e4ad1ef4df2c060
Ryan The <ryan.the.2006@gmail.com>
2020-03-11 21:37:29 +0800

    Add icons to tabs

--- App.tsx
+++ App.tsx
@@ -8,20 +8,13 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
+import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { Provider } from 'react-redux';
-import { reducer } from './src/store/reducer';
-import { createStore } from 'redux'
+import {Provider} from 'react-redux';
+import {reducer} from './src/store/reducer';
+import {createStore} from 'redux';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import {HomePage} from './src/pages/Home';
 import {SettingsPage} from './src/pages/Settings';
@@ -29,15 +22,37 @@ import {SettingsPage} from './src/pages/Settings';
 const store = createStore(reducer);
 
 const Tab = createBottomTabNavigator();
-const App: () => React$Node = () => {
+
+const App = () => {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Settings" component={SettingsPage} />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              switch (route.name) {
+                case "Home":
+                  iconName = focused ? 'home-variant' : 'home-variant-outline';
+                  break;
+                case 'Settings':
+                  iconName = focused ? 'settings' : 'settings-outline';
+                  break;
+                default:
+                  iconName = 'alert-circle';
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen name="Home" component={HomePage} />
+          <Tab.Screen name="Settings" component={SettingsPage} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
