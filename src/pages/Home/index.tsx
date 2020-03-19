16050c6c730fd05c54fd09c2e243d04f418540e9
Ryan The <ryan.the.2006@gmail.com>
2020-03-19 19:46:50 +0800

    Add modals for Settings page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Layout, Text} from '@ui-kitten/components';
-import {phonecall} from 'react-native-communications';
 
 import {PageHeader} from '../../components/Page/PageHeader';
-import {PageList} from '../../components/Page/PageList';
 import {ViewShadow} from '../../components/Shadow/View';
 
 const HomePageC = (props: any) => {
