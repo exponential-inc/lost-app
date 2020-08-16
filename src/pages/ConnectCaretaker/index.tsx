6242d478c00546c145005b1615721a48231ee230
Xavier3372 <koh_jie_ming_xavier@s2019.ssts.edu.sg>
2020-08-16 12:06:07 +0800

    code system initial commit

--- src/pages/ConnectCaretaker/index.tsx
+++ src/pages/ConnectCaretaker/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Layout, Text} from '@ui-kitten/components';
 import {connect} from 'react-redux';
+
 
 import {LoginButton} from '../../components/Button/Login';
 import {PageHeader} from '../../components/Page/PageHeader';
 import {K} from '../../store/constants';
 import Svg, {Circle, G, Path, Defs} from 'react-native-svg';
-import {View} from 'react-native';
+import {View, TextInput, Alert} from 'react-native';
 import firebase from '@react-native-firebase/app'
 import database from '@react-native-firebase/database'
 import auth from '@react-native-firebase/auth';
@@ -14,6 +15,7 @@ import base64 from 'react-native-base64'
 
 
 const ConnectCaretakerPageC = (props: any) => {
+  const [text, setText] = useState('');
   const themeColor = props.theme === 'dark' ? K.colors.dark : K.colors.light;
   const globalAny:any = global;
   console.log(props.deviceSize.width);
@@ -21,8 +23,20 @@ const ConnectCaretakerPageC = (props: any) => {
   const dbRef = `/caretakers/${base64.encode(`${user.email}`)}`
   globalAny.type = 'caretaker'
 
-  
-
+  const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Incorrect code",
+      "Please check your code and try again",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
   const SvgBackgroundGraphic = (svgProps: {isLarge?: boolean}) => (
     <View style={{position: 'absolute'}}>
       <Svg
@@ -49,9 +63,47 @@ const ConnectCaretakerPageC = (props: any) => {
       </Svg>
     </View>
   );
-
-
-  return null;
+    
+  return (
+    <Layout style={{height: '100%', backgroundColor: themeColor.linkBG}}>
+    <SvgBackgroundGraphic isLarge />
+  <SvgBackgroundGraphic />
+  <View style={{justifyContent: "center", height: '100%', marginTop: 100}}>
+  <TextInput
+        style={{height: 40}}
+        placeholder="Type the code here"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+      />
+  <LoginButton
+          title="Next"
+          theme={props.theme}
+          onPress={() => database()
+            .ref(`codeStore/${`${text}`}`)
+            .once('value')
+            .then(snapshot => {
+              if (snapshot.exists()) {
+                console.log(`codestore/${`${text}`}`);
+                database()
+              .ref(dbRef)
+              .set({
+                email: user.email,
+                name: user.displayName,
+                connectedPatient: database()
+                .ref(`/codeStore/${`${text}`}/email`)
+                .once('value')
+              }).then(props.navigation.navigate('Home'))
+                console.log(snapshot.val());
+              } else {
+                {createTwoButtonAlert}
+                  }
+                }  
+                )
+              }  
+        />
+  </View>
+</Layout>
+  );
 };
 
 
